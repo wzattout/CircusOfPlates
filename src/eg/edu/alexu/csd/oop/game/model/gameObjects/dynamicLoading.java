de8ad6b6446bafb93e846a1a83ce4e0a06d7da2a
Walid Ahmed Zattout <wzattout@gmail.com>
2019-12-13 14:20:09 +0200

    dynamic loading

---
+++ src/eg/edu/alexu/csd/oop/game/model/gameObjects/dynamicLoading.java
@@ -0,0 +1,27 @@
+package eg.edu.alexu.csd.oop.game.model.gameObjects;
+
+import javax.swing.*;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class dynamicLoading {
+    public Class dynamicLoad() {
+        JFileChooser fileChooser = new JFileChooser();
+        int returnValue = fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            try {
+                URL url = selectedFile.toURI().toURL();
+                URL[] urls = new URL[]{url};
+                ClassLoader cl = new URLClassLoader(urls);
+                Class cls = cl.loadClass("external shape");
+                return cls;
+            } catch (MalformedURLException e) {
+            } catch (ClassNotFoundException e) {
+            }
+        }
+        return null;
+    }
+}
