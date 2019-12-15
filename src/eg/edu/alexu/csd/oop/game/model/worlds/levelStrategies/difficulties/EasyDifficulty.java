5df1efe32294bbe2f32100b2d70ad14222b6c33c
Newbie-Pi <maroashraf428@gmail.com>
2019-12-15 21:58:21 +0200

    Three Difficulties & Arcade Mode Created(Awaiting Implementation)

---
+++ src/eg/edu/alexu/csd/oop/game/model/worlds/levelStrategies/difficulties/EasyDifficulty.java
@@ -0,0 +1,4 @@
+package eg.edu.alexu.csd.oop.game.model.worlds.levelStrategies.difficulties;
+
+public class EasyDifficulty implements Difficulty {
+}
---
+++ src/eg/edu/alexu/csd/oop/game/model/worlds/levelStrategies/modes/ArcadeMode.java
@@ -0,0 +1,21 @@
+package eg.edu.alexu.csd.oop.game.model.worlds.levelStrategies.modes;
+
+import eg.edu.alexu.csd.oop.game.model.utils.Score;
+
+public class ArcadeMode implements Mode {
+
+    private long timeElapsed;
+    private int remainingLives = 3;
+    private long startTime = System.currentTimeMillis();
+
+    @Override
+    public boolean refresh() {
+        timeElapsed = System.currentTimeMillis() - startTime;
+        return remainingLives > 0;
+    }
+
+    @Override
+    public String getStatus() {
+        return "Score: " + Score.getInstance().getScore() + " Time Elapsed: " + timeElapsed/1000 + " Remaining Lives: " + remainingLives;
+    }
+}
