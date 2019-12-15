a525718bedc11ff6002d9c905715f008d8e49fd7
Islam Yousry <islamyousry16@gmail.com>
2019-12-15 21:43:53 +0000

    SnapShot.java

--- src/eg/edu/alexu/csd/oop/game/model/utils/SnapShot.java
+++ src/eg/edu/alexu/csd/oop/game/model/utils/SnapShot.java
@@ -1,4 +1,18 @@
 package eg.edu.alexu.csd.oop.game.model.utils;
 
+import eg.edu.alexu.csd.oop.game.model.worlds.levelStrategies.modes.Mode;
+
 public class SnapShot {
+
+    private static Mode mode = null;
+
+    public SnapShot(Mode mode){
+        this.mode = mode;
+    }
+
+
+    public Mode getState(){
+        return this.mode;
+    }
+
 }
